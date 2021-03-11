0dd376df8c884fbdf9ed9a33c145204c12ac8d66
JUCSERahull337 <45335164+JUCSERahull337@users.noreply.github.com>
2021-03-11 20:27:56 +0600

    readme updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 //import { faYoutube } from '@fortawesome/free-solid-svg-icons'
-import { faFacebookF,faInstagram,faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF,faTwitter } from '@fortawesome/free-brands-svg-icons'
 import './Footer.css'
 //import { Link } from 'react-router-dom';
 
@@ -10,9 +10,8 @@ const Footer = (props) => {
     
     return (
         <div className='footer'>
-            <a href={strFacebook} target="_blank"><FontAwesomeIcon className='Icon' icon={faFacebookF}/></a>
-            <a href={strTwitter} target="_blank"><FontAwesomeIcon className='Icon' icon={faTwitter}/></a>
-            <a href={strFacebook} target="_blank"><FontAwesomeIcon className='Icon' icon={faInstagram}/></a>
+            <a href={`https://${strFacebook}`} target="_blank"><FontAwesomeIcon className='Icon' icon={faFacebookF}/></a>
+            <a href={`https://${strTwitter}`} target="_blank"><FontAwesomeIcon className='Icon' icon={faTwitter}/></a>
            
            {/* <Link to=''><FontAwesomeIcon className='Icon' icon={faTwitter}/></Link>
             <Link to='www.instagram.com/chelseafc'><FontAwesomeIcon icon={faInstagram}/></Link>  */}
